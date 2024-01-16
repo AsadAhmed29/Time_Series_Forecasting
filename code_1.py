23e03589f04975cd18cc5b7cb072cf9db0dc6882
AsadAhmed29 <asad.nedian29@gmail.com>
2024-01-16 07:51:54 -0800

    Data pre processing and Building of LSTM Model for univariate prediction

--- code_1.py
+++ code_1.py
@@ -2,6 +2,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import tensorflow as tf 
+print(tf.version)
+from tensorflow.keras.models import load_model
+
+
 
 df = pd.read_csv('Amazon Data.csv')
 
@@ -55,3 +61,58 @@ plt.xlabel('Time')
 plt.ylabel('Daily Lowesr trading Price ')
 plt.title('Trading Lows Over the Years')
 plt.show()# Shows trends for Daily highest trading price over the years
+
+#
+# UNIVARIATE PREDICTION USING LSTM
+#
+
+# DATA PRE PROCESSING#
+
+one_feature_df = df.filter(['Open']) 
+one_feature_array = one_feature_df.values # converting into an array
+training_array_len  = np.int64(len(one_feature_array)*0.8)
+
+
+#Scaling
+scaler = MinMaxScaler(feature_range=(0,1))                               
+scaled_array = scaler.fit_transform(one_feature_array)
+
+#Spliting the Data
+training_data = scaled_array[0:training_array_len,0]
+training_days = 45
+x_train = []
+y_train = []
+
+for i in range(training_days,training_array_len):
+  x_train.append(training_data[i-training_days:i])
+  y_train.append(training_data[i])
+
+
+#converting to numpy arrays
+x_train = np.array(x_train)
+y_train = np.array(y_train)
+
+#Reshaping into 3D for LSTM
+x_train = np.reshape(x_train, (x_train.shape[0], training_days, 1))
+
+
+#BUILDING THE MODEL
+
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+
+
+model = Sequential()
+model.add(LSTM(50, return_sequences = True , input_shape = (training_days,1 ) ) )
+model.add(LSTM(50, return_sequences = False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+#Compiling
+model.compile( optimizer = 'adam' , loss = 'mean_squared_error')
+
+#Training 
+#model.fit(x_train , y_train , batch_size = 1, epochs =10)       #trained on Google Colab
+
+# Load the saved model
+open_model = load_model('Open_trained_model.h5')
