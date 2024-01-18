9130d115f65d45cb347b99ab077880d458f0c071
AsadAhmed29 <asad.nedian29@gmail.com>
2024-01-18 04:47:43 -0800

    Creating a function for validating and plotting model

--- code_1.py
+++ code_1.py
@@ -65,84 +65,95 @@ df['Volume'] = df['Volume'].fillna(df['Volume'].rolling(window=5, center=True , 
 # UNIVARIATE PREDICTION USING LSTM
 #
 
-# DATA PRE PROCESSING#
+# # DATA PRE PROCESSING#
 
-one_feature_df = df.filter(['Open']) 
-one_feature_array = one_feature_df.values # converting into an array
-training_array_len  = np.int64(len(one_feature_array)*0.8)
+def uni_variate_training(target):
+  one_feature_df = df.filter([target]) #filtering
+  one_feature_array = one_feature_df.values # converting into an array
+  training_array_len  = np.int64(len(one_feature_array)*0.8)
+  from sklearn.preprocessing import MinMaxScaler
+  #Scaling the Data
 
+  scaler = MinMaxScaler(feature_range=(0,1))
+  scaled_array = scaler.fit_transform(one_feature_array)
 
-#Scaling
-scaler = MinMaxScaler(feature_range=(0,1))                               
-scaled_array = scaler.fit_transform(one_feature_array)
+  #spliting the Data
+  training_data = scaled_array[0:training_array_len,0]
+  training_days = 45
+  x_train = []
+  y_train = []
 
-#Spliting the Data
-training_data = scaled_array[0:training_array_len,0]
-training_days = 45
-x_train = []
-y_train = []
+  for i in range(training_days,training_array_len):
+    x_train.append(training_data[i-training_days:i])
+    y_train.append(training_data[i])
 
-for i in range(training_days,training_array_len):
-  x_train.append(training_data[i-training_days:i])
-  y_train.append(training_data[i])
+  #converting to numpy arrays
+  x_train = np.array(x_train)
+  y_train = np.array(y_train)
 
-
-#converting to numpy arrays
-x_train = np.array(x_train)
-y_train = np.array(y_train)
-
-#Reshaping into 3D for LSTM
-x_train = np.reshape(x_train, (x_train.shape[0], training_days, 1))
-
-
-#BUILDING THE MODEL
-
-from keras.models import Sequential
-from keras.layers import Dense, LSTM
+  x_train = np.reshape(x_train, (x_train.shape[0], training_days, 1))
 
 
-model = Sequential()
-model.add(LSTM(50, return_sequences = True , input_shape = (training_days,1 ) ) )
-model.add(LSTM(50, return_sequences = False))
-model.add(Dense(25))
-model.add(Dense(1))
-
-# Load the saved model
-open_model = load_model('Open_trained_model.h5')
-
-#Creating test data
-test_array = scaled_array[training_array_len-training_days: ,: ]
-
-x_test = []
-y_test = one_feature_array[training_array_len: ,:]
-len(y_test)
-
-for i in range(training_days, len(test_array)):
-  x_test.append(test_array[i-training_days:i])
-
-x_test = np.array(x_test) #Converting into array
-x_test = np.reshape(x_test , (x_test.shape[0], x_test.shape[1], 1)) #Reshaping into 3D
-
-# #Making Predictions
-# predictions = open_model.predict(x_test)
-# predictions = scaler.inverse_transform(predictions)
-
-# rmse = np.sqrt(np.mean(predictions-y_test)**2)
-# accuracy = rmse
-
-# #Plotting
-# import matplotlib.pyplot as plt
-
-# train_data_actual = one_feature_df[:training_array_len]
-# test_data_actual = one_feature_df[training_array_len:]
-# test_data_actual['Predictions'] = predictions
+  #Building the model
+  from keras.models import Sequential
+  from keras.layers import Dense, LSTM
 
 
-# plt.figure(figsize = (12,8))
-# plt.xlabel('Date')
-# plt.ylabel('Open Price USD$')
-# plt.plot(train_data_actual , label = 'Training data')
-# plt.plot(test_data_actual[['Open' , 'Predictions']] , label = ['Actual test values' , 'Predicted Test Values'] )
-# plt.legend()
-# plt.show()
-print('5')
+  model = Sequential()
+  model.add(LSTM(50, return_sequences = True , input_shape = (training_days,1 ) ) )
+  model.add(LSTM(50, return_sequences = False))
+  model.add(Dense(25))
+  model.add(Dense(1))
+
+  #Compile the model
+  model.compile( optimizer = 'adam' , loss = 'mean_squared_error')
+
+  #Training the model
+  model.fit(x_train , y_train , batch_size = 1, epochs =10)
+
+  return model
+
+def validate_and_plot(target ,model, ylabel):
+  one_feature_df = df.filter([target]) #filtering
+  one_feature_array = one_feature_df.values # converting into an array
+  training_array_len  = np.int64(len(one_feature_array)*0.8)
+
+  from sklearn.preprocessing import MinMaxScaler
+
+  scaler = MinMaxScaler(feature_range=(0,1))
+
+  scaled_array = scaler.fit_transform(one_feature_array)
+  test_array = scaled_array[training_array_len-45: ,: ]
+
+  x_test = []
+  y_test = one_feature_array[training_array_len: ,:]
+
+  for i in range(45, len(test_array)):
+    x_test.append(test_array[i-45:i])
+
+  x_test = np.array(x_test) #Converting into array
+  x_test = np.reshape(x_test , (x_test.shape[0], x_test.shape[1], 1)) #Reshaping into 3D
+
+  #Predictions
+  predictions = model.predict(x_test)
+  predictions = scaler.inverse_transform(predictions)
+
+  #Getting Rmse
+  rmse = np.sqrt(np.mean(predictions-y_test)**2)
+
+
+  train_data_actual = one_feature_df[:training_array_len]
+  test_data_actual = one_feature_df[training_array_len:]
+  test_data_actual['Predictions'] = predictions
+
+
+  plt.figure(figsize = (12,8))
+  plt.xlabel('Date')
+  plt.ylabel(ylabel)
+  plt.plot(train_data_actual , label = 'Training data')
+  plt.plot(test_data_actual[[target , 'Predictions']] , label = ['Actual test values' , 'Predicted Test Values'] )
+  plt.legend()
+  return plt.show()
+
+
+
